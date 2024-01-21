38ffea4de4c759256a67468a4067666723c1621a
respowerr <respowerryt@gmail.com>
2024-01-21 20:53:32 +0100

    Project init

---
+++ build/CMakeFiles/discovery.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/discovery.dir/src/main.c.obj"
+  "CMakeFiles/discovery.dir/src/main.c.obj.d"
+  "discovery.exe"
+  "discovery.exe.manifest"
+  "discovery.pdb"
+  "libdiscovery.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/discovery.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
