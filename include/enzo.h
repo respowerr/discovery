da84fa4f328caf1ea3ba2516050dff06f2785e33
SnikiEP <enzopizzuti17@hotmail.fr>
2024-01-25 10:15:08 +0100

    rm

--- include/enzo.h
+++
@@ -1,8 +0,0 @@
-#ifndef ENZO_H
-#define ENZO_H
-
-#include <gtk/gtk.h>
-
-void run_enzo_panel(int argc, char *argv[]);
-
-#endif // ENZO_H
--- include/verif_robot.h
+++
@@ -1,6 +0,0 @@
-#ifndef VERIF_ROBOT_H
-#define VERIF_ROBOT_H
-
-void robot_query_robot_txt(const char *url, char *response, size_t response_size);
-
-#endif
